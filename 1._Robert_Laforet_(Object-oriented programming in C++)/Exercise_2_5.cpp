b8a711a9b301fc22241e3d988f4358a5e44bf42d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-17 23:35:20 +0300

    Delete 1._Robert_Laforet_(Object-oriented programming in C++)/Exercise_2_5.cpp

--- 1._Robert_Laforet_(Object-oriented programming in C++)/Exercise_2_5.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-void main() {
-	setlocale(LC_ALL, "Rus");
-	char a;
-	cout << endl << "Введите букву: ";
-	cin >> a;
-	cout << endl << "\t" << islower(a) << endl;
-}
