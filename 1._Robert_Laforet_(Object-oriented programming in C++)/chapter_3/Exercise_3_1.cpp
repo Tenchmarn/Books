e2f980bd641291e0721556f2c97cb069cd31d5cd
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-18 00:51:28 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "RUS");
+	int s;
+	cout << "Введите число: "; cin >> s;
+	for (int i = 1; i <= 200; i++)
+	{
+		cout << i * s << "";
+		if (i % 10 == 0) cout << endl;
+	}
+}
