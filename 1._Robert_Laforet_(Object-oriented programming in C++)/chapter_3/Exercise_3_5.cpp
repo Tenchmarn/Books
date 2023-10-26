8c4364fe36e3c04ec15c114284c804f0fa33ed50
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-27 02:40:31 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	
+	int a = 20;
+	int TT_0 = 2 * a - 1;
+	for (int i = 0; i <= a; i++) {
+		int a_0 = TT_0 - (2 * i - 1);
+		for (int j = 0; j <= TT_0; j++) {
+			cout << ((j <= a_0) ? " " : "x");
+		}
+		cout << endl;
+	}
+	return 0;
+}
