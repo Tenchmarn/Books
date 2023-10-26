b3f7bb9baa4915e3cc78662ed8767ef0ef7d3a9a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-27 00:49:36 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	char a;
+	unsigned long TT_0 = 0;
+	cout << "\nВведите число: "; 
+	while ((a = getchar()) != '\r') {
+		TT_0 = TT_0 * 10 + a - '0';
+		if (TT_0 ==123456) {
+			cout << "\nПолучилось число: " << TT_0  << endl;
+		}
+	}
+	return 0;
+}
