1d474e2252531ce45b953b2e7afa7087adf837d9
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-28 15:37:20 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int numb;
+	long fact = 1;
+	while (true) {
+		cout << "\n\tВведите целое число: ";
+		cin >> numb;
+		if (numb == 0) {
+			cout << "\n\tОшибка, неправильный ввод данных!"
+				<< "\n\tПерезапустите программу." << endl;
+			break;
+		}
+		else {
+			for (int i = numb; i > 0; i--) {
+				fact *= i;
+			}
+			cout << "\tФакториал числа равен: " << fact << endl;
+			fact = 1;
+		}
+	}
+	return 0;
+}
