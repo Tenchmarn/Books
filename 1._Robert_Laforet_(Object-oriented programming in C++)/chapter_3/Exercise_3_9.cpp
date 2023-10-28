a05ca121729e5f6ea6b72feb445beb81c2149d0c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-28 18:06:50 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int guests_uuu, seat_table, rezult = 1;
+	cout << "\n\tВведите число гостей: "; cin >> guests_uuu;
+	cout << "\tВведите число мест за столом: "; cin >> seat_table;
+	while (guests_uuu < seat_table) {
+		cout << "\n\tЧисло гостей не должно быть меньше числа мест за столом!" << endl;
+		cout << "\tВведите число гостей: "; cin >> guests_uuu;
+	}
+	for (int i = 1; i <= seat_table; i++) {
+		rezult *= guests_uuu;
+		guests_uuu -= 1;
+	}
+	cout << "\n\tЧисло возможных рассадок гостей за столом: " << rezult << endl;
+
+	return 0;
+}
