8f4c68993678796106b2f6a545c6f193d1791793
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-10-28 18:37:27 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_3/Exercise_3_10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	float sum, expected_contribution;
+	float perc, ye;
+	cout << "\n\tВведите начальный вклад: "; cin >> sum;
+	cout << "\tВведите процентную ставку: "; cin >> perc;
+	cout << "\tВведите требуемый вклад: "; cin >> expected_contribution;
+	perc = perc / 100;
+	for (int i = 1;; i++) {
+		sum = sum + (sum * perc);
+		ye = i;
+		if (sum >= expected_contribution) {
+			break;
+		}
+	}
+	cout << "\tДля достижения требуемого вклада, потребуется " << ye << " лет.\n";
+	return 0;
+}
