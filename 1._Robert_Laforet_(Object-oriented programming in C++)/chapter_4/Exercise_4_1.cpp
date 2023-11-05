f9ea18e44cfaa0bdbaa153df85f4720493ab20e0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-06 00:51:06 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+struct phone {
+	int city_code;
+	int station_number;
+	int subscriber_number;
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	phone phone_1, phone_2;
+	cout << "Введите код города, номер станции и номер абонента: ";
+	cin >> phone_2.city_code >>
+		phone_2.station_number >>
+		phone_2.subscriber_number;
+	phone_1.city_code = 212;
+	phone_1.station_number = 767;
+	phone_1.subscriber_number = 8900;
+	cout << "Мой номер:" << " (" << phone_1.city_code
+		<< ") " << phone_1.station_number
+		<< "-" << phone_1.subscriber_number
+		<< endl;
+	cout << "Ваш номер:" << " (" << phone_2.city_code 
+		<< ") " << phone_2.station_number 
+		<< "-" << phone_2.subscriber_number
+		<< endl;
+	return 0;
+}
---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+struct point {
+	int x, y;
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	point p1, p2;
+	cout << "Введите координаты точки p1: "; cin >> p1.x >> p1.y;
+	cout << "Введите координаты точки p2: "; cin >> p2.x >> p2.y;
+	cout << "Координаты точки p1 + p2 равны " << p1.x + p2.x << "." << p1.y + p2.y;
+	return 0;
+}
