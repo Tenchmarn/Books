71a0684e7cf941ef971c70935786c13587ddcee0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-05 23:43:09 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/examples/wdcount.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+enum itsaWord {NO, YES};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	itsaWord isWord = NO;
+	char ch;
+	int wordcount = 0;
+	cout << "Введите предложение: \n";
+	do {
+		ch = getchar();
+		if (ch == ' ' || ch == '\r') {
+			if (isWord == YES) {
+				wordcount++;
+				isWord = NO;
+			}
+		}
+		else
+			if (isWord == NO) {
+				isWord = YES;
+			}
+	} while (ch != '\r');
+	cout << "\n--Число слов: " << wordcount << "---\n";
+	return 0;
+}
