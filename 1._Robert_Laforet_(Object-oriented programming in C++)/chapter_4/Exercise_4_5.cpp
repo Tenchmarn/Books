4cc3f608642c1ced337dcb788265965b1d837fbc
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-07 12:55:12 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+struct date {
+	int day; // день
+	int month; // месяц
+	int year; // год
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	char C_0 = '/';
+	date D_1;
+	cout << "Введите дату (день, месяц, год): ";
+	cin >> D_1.day >> C_0 >> D_1.month >> C_0 >> D_1.year;
+	cout << "Дата: " << D_1.day << "/" << D_1.month << "/" << D_1.year << endl;
+
+	return 0;
+}
