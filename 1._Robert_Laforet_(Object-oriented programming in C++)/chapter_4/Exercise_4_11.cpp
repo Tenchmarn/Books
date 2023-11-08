0d8743a11cb19a64c7b806b3bed81a2c7d7249bc
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-09 02:13:24 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+struct timeStruct {
+	int hours;
+	int minutes;
+	int second;
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	timeStruct time_1, time_2, time_sum_0;
+	char A_0;
+	cout << "\n\tВведите время 1, формата (часы:минуты:секунды): ";
+	cin >> time_1.hours >> A_0 >> time_1.minutes >> A_0 >> time_1.second;
+	cout << "\tВведите время 2, формата (часы:минуты:секунды): ";
+	cin >> time_2.hours >> A_0 >> time_2.minutes >> A_0 >> time_2.second;
+	int totalseсs_0 = time_1.hours * 3600 + time_1.minutes * 60 + time_1.second;
+	int totalseсs_1 = time_2.hours * 3600 + time_2.minutes * 60 + time_2.second;
+	int sumSecond = totalseсs_0 + totalseсs_1;
+	cout << "\n\tРезультат сложения времени в секундах: " << sumSecond;
+	time_sum_0.hours = sumSecond / 3600;
+	time_sum_0.minutes = (sumSecond % 3600) / 60;
+	time_sum_0.second = (sumSecond % 3600) % 60;
+	cout << "\n\tСумма сложения времени, 1 и 2: "
+		<< time_sum_0.hours << ":"
+		<< time_sum_0.minutes << ":"
+		<< time_sum_0.second << endl;
+	return 0;
+}
+
