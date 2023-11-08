4bfc45aa588148436cc95b1390f718e161060e5f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-09 01:02:55 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+struct sterling {
+	int pounds; // фунты
+	int shillings; // шиллинги
+	int pance; // пенсы
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	sterling s_0;
+	double ATT_0;
+	cout << "Введите сумму в фунтах: ";
+	cin >> ATT_0;
+	s_0.pounds = ATT_0;
+	s_0.shillings = (ATT_0 - s_0.pounds) * 20;
+	s_0.pance = ((ATT_0 - s_0.pounds) * 20 - s_0.shillings) * 12;
+	cout << "Результат: "
+		<< s_0.pounds << " фунтов, "
+		<< s_0.shillings << " шиллингов, "
+		<< s_0.pance << " пенсов "
+		<< endl;
+	return 0;
+}
