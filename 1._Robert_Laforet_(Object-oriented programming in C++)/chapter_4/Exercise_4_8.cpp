e15da8edeb9638b1d0f034c41838b27e30f205e3
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-08 16:00:39 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+struct fraction {
+	int numerator; // числитель
+	int denominator; // знаменатель
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	fraction fraction_1, fraction_2;
+	char A_0 = '/';
+	cout << "\n\tВведите дробь 1 формата (числитеть/знаменатель): ";
+	cin >> fraction_1.numerator >> A_0 >> fraction_1.denominator;
+	cout << "\n\tВведите дробь 2 формата (числитель/знаменатель): ";
+	cin >> fraction_2.numerator >> A_0 >> fraction_2.denominator;
+	cout << "\n\tСумма равна: "
+		<< ((fraction_1.numerator* fraction_2.denominator) + (fraction_2.numerator* fraction_1.denominator))
+		<< A_0 << (fraction_1.denominator* fraction_2.denominator) << endl;
+	return 0;
+}
