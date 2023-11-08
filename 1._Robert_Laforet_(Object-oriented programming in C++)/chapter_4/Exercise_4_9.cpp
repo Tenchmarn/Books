cbe56f001d45df2a11198c6e47feaac26ee02674
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-08 16:47:55 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_4/Exercise_4_9.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+struct timeStruct {
+	int hours; // часы 
+	int minutes; // минуты
+	int seconds; // секунды
+};
+int main() {
+	setlocale(LC_ALL, "RUS");
+	timeStruct t_1;
+	char A_0;
+	cout << "\n\tВведите время формата (часы/минуты/секунды): ";
+	cin >> t_1.hours >> A_0 >> t_1.minutes >> A_0 >> t_1.seconds;
+	int totalseсs = t_1.hours * 3600 + t_1.minutes * 60 + t_1.seconds;
+	cout << "\tВсего секунд: " << totalseсs << endl;
+
+	return 0;
+}
