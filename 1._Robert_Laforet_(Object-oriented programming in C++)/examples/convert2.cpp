0508748d5348080c755d10681ec7caf066187607
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-12 13:23:51 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/examples/convert2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+float lbstokg(float);
+int main() {
+	setlocale(LC_ALL, "RUS");
+	float lbs;
+	cout << "\nВведите вес в фунтах: ";
+	cin >> lbs;
+	cout << "Вес в килограммах равен " << lbstokg(lbs) << endl;
+	return 0;
+}
+float lbstokg(float pounds) {
+	return 0.453592 * pounds;
+}
