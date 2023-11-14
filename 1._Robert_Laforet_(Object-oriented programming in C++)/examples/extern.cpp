9e159470ebec3fa7acb6ca0bd996f3022fde53e0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-15 01:00:14 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/examples/extern.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+char  ch = 'a';
+void getachar();
+void putachar();
+int main() {
+	while (ch != '\r') {
+		getachar();
+		putachar();
+	}
+	cout << endl;
+	return 0;
+}
+void getachar() {
+	cin >> ch;
+}
+void putachar() {
+	cout << ch;
+}
