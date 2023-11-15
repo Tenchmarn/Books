aedf3b691e1bc683faf3c9826fe1801325ff5f2b
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-15 17:51:30 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/examples/retref.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int x;
+inline int& setx();
+int main() {
+	setx() = 92;
+	cout << "x = " << x << endl;
+	return 0;
+}
+inline int& setx() {
+	return x;
+}
