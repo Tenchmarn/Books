df6abcf97846cbccefec663d826932f743046eab
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-26 07:52:59 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/tasks/Exercise_5_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+inline float Fun_circarea(float n);
+int main() {
+	setlocale(LC_ALL, "RUS");
+	float rad;
+	cout << "Введите радиус окружности: "; cin >> rad;
+	cout << "Площадь круга равна: " << Fun_circarea(rad);
+	cout << endl;
+	return 0;
+}
+inline float Fun_circarea(float n) {
+	const float PI = 3.14159F;
+	return PI * n * n;
+}
---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/tasks/Exercise_5_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+inline double power(double n, int p = 2);
+int main() {
+	setlocale(LC_ALL, "RUS");
+	double N_0, N_1; // число
+	int P_0; // степень
+	char char_A;
+	cout << "Введите число: "; cin >> N_0;
+	cout << "Ввод сепени (y/n): "; cin >> char_A;
+	if (char_A == 'y') {
+		cout << "Введите степень: "; cin >> P_0;
+		N_1 = power(N_0, P_0);
+	}
+	else {
+		N_1 = power(N_0);
+	}
+	cout << "Результат равен: " << N_1;
+	return 0;
+}
+inline double power(double n, int p) {
+	double sum = 1.0;
+	for (int i = 0; i < p; i++) {
+		sum *= n;
+	}
+	return sum;
+}
