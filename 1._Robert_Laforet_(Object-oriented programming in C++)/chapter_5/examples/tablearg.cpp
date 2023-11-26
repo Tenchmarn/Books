9cf0aa9851cba69bd495209f00a7b150cb7ea3d4
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-26 07:58:29 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/examples/tablearg.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void repchar(char, int); // прототип функции
+int main() {
+	setlocale(LC_ALL, "RUS");
+	repchar('-', 43);
+	cout << "Тип данных  Диапазон" << endl;
+	repchar('=', 23);
+	cout << "char    -128...127" << endl
+		<< "short   -32 768...32 767" << endl
+		<< "int     Системно-зависимый" << endl
+		<< "long    -2 147 483 648...2 147 483 647" << endl;
+	repchar('-', 43);
+	return 0;
+}
+void repchar(char ch, int n) {
+	for (int j = 0; j < n;j++) {
+		cout << ch;
+	}
+	cout << endl;
+}
