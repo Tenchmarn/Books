69b1d2e1b8520fbcde1f0ab5c124ece116b24601
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-26 07:58:16 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/examples/table2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void starline() {
+	for (int j = 0; j < 45; j++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+int main() {
+	setlocale(LC_ALL, "RUS");
+	cout << endl;
+	starline();
+	cout << "Тип данных Диапазон" << endl;
+	starline();
+	cout << "char    -128...127" << endl
+		<< "short   -32 768...32 767" << endl
+		<< "int     Системно-зависимый" << endl
+		<< "long    -2 147 483 648...2 147 483 647" << endl;
+	starline();
+	return 0;
+}
