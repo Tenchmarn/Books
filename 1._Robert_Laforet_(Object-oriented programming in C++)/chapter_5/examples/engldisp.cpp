0092e7d34655fc5c1ab1fae45c806962cfc727d5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-26 07:53:51 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/examples/engldisp.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+struct Distance {
+	int feet;
+	float inches;
+};
+void engldisp(Distance);
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	Distance d1, d2;
+	cout << "Введите число футов: "; cin >> d1.feet;
+	cout << "Введите число дюймов: "; cin >> d1.inches;
+	cout << "Введите число футов: "; cin >> d2.feet;
+	cout << "Введите число дюймов: "; cin >> d2.inches;
+	cout << "d1= "; 
+	engldisp(d1);
+	cout << "d2= "; 
+	engldisp(d2);
+	return 0;
+}
+void engldisp(Distance dd) {
+	cout << dd.feet << "\'-" << dd.inches << "\"" << endl;
+}
