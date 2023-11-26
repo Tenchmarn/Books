6dfbeccd555af94566ea5ed37bc372f10c5dbdc4
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-26 07:56:17 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/examples/ref.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	void intfrac(float, float &, float &); // прототип
+	float number, intpart, fracpart;
+	do {
+		cout << "\nВведите вещественное число: ";
+		cin >> number;
+		intfrac(number, intpart, fracpart);
+		cout << "Целая часть равна " << intpart
+			<< ", дробная часть равна " << fracpart << endl;
+	} while (number != 0.0);
+	return 0;
+}
+void intfrac(float n, float& intp, float& fracp) {
+	long temp = static_cast<long>(n); // преобразование к типу long
+	intp = static_cast<float>(temp); // преобразование к типу float
+	fracp = n - intp;
+}
