7c6550dda91277e268725b2d9b8648c0ae4dff22
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-12-01 00:17:38 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/tasks/Exercise_5_3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+void zeroSmaller(int&, int&);
+int main() {
+	int A_1 = 5, A_2 = 8;
+	int M_1 = 7, M_2 = 3;
+	zeroSmaller(A_1, A_2);
+	zeroSmaller(M_1, M_2);
+	return 0;
+}
+void zeroSmaller(int& T_1, int& T_2) {
+	if (T_1 < T_2) {
+		T_1 = 0;
+	}
+	else {
+		T_2 = 0;
+	}
+}
