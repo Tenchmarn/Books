2222dff93268049edcc7c426695b31f9bcfb1049
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-12-02 02:52:45 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_5/tasks/Exercise_5_8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int&, int&);
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int A_1, A_2;
+	cout << "\n\tВведите 2 числа." << endl;
+	cout << "- A_1: "; cin >> A_1;
+	cout << "- A_2: "; cin >> A_2;
+	cout << "\tМеняем числа местами." << endl;
+	swap(A_1, A_2);
+	cout << "- A_1: " << A_1 << endl;
+	cout << "- A_2: " << A_2 << endl;
+	return 0;
+}
+void swap(int& n1, int& n2) {
+	int res;
+	res = n1;
+	n1 = n2;
+	n2 = res;
+}
