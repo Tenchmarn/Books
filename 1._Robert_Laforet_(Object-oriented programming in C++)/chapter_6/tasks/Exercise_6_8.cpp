89dc99f9ff505b603d26f6b189e1976a9031383a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-01-03 02:49:10 +0300

    Add files via upload

---
+++ 1._Robert_Laforet_(Object-oriented programming in C++)/chapter_6/tasks/Exercise_6_8.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+class SClass_objects {
+private:
+	int serial_number; // содержит порядковый номер созданного объекта, отсчет от 1
+	static int total_objects; // счетчик объектов
+public:
+	SClass_objects() { 
+		total_objects++; 
+		serial_number = total_objects;
+	}
+	void Fun_displayNumber() {
+		cout << "\n\t\tМой порядковый номер: " << serial_number << endl;
+	}
+};
+int SClass_objects::total_objects = 0;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	SClass_objects d1, d2, d3;
+	d1.Fun_displayNumber();
+	d2.Fun_displayNumber();
+	d3.Fun_displayNumber();
+	return 0;
+}
