f58aaeedb071ee9ebca5a838726d243baffeabdb
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-06-14 21:07:05 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_1/example_3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int first, second, sum;
+	cout << "Введите два целых числа: ";
+	cin >> first >> second;
+	sum = first + second;
+	cout << first << " + " << second << " = " << sum << endl;
+	return 0;
+}
