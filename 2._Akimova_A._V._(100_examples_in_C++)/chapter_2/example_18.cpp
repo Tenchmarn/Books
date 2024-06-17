812d0f03c6a89b96ddf53e79c9862579d94b13b2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-06-18 01:11:47 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_2/example_18.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	/*
+	int n1, n2, LCM;
+	cout << "Введите два целых числа: ";
+	cin >> n1 >> n2;
+	//			определяем максимум среди n1 и n2 и сохраняем в LCM;
+	LCM = (n1 > n2) ? n1 : n2;
+	//			бесконечный цикл
+	while (1) {
+		if (LCM % n1 == 0 && LCM % n2 == 0) {
+			cout << "НОК = " << LCM << "\n";
+			break;
+		}
+		++LCM;
+	}
+	cout << endl;
+	*/
+	int n1, n2, hcf, temp, lcm;
+	cout << "Введите два числа: ";
+	cin >> n1 >> n2;
+	hcf = n1;
+	temp = n2;
+	while (hcf != temp) {
+		if (hcf > temp) {
+			hcf -= temp;
+		}
+		else {
+			temp -= hcf;
+		}
+	}
+	lcm = (n1 * n2) / hcf;
+	cout << "НОК = " << lcm;
+	return 0;
+}
