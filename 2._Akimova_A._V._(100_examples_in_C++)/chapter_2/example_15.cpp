6ef47023c6d845716669815b7c873b9031efa8f3
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-06-17 22:08:36 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_2/example_15.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int i, j;
+	cout << endl;
+	for (i = 1; i <= 9; i++) {
+		for (j = 1; j <= 9; j++) {
+			cout.width(5);
+			cout << i * j << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
