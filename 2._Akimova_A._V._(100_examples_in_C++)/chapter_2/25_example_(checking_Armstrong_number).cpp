ebb1dce1cfd6dc4a845f0260f00e4daf619adc09
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-07-01 16:05:56 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_2/25_example_(checking_Armstrong_number).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int origNum, num, rem, digit, sum = 0;
+	cout << "Введите целое положительное число: ";
+	cin >> origNum;
+	num = origNum;
+	while (num != 0) {
+		digit = num % 10;
+		sum += digit * digit * digit;
+		num /= 10;
+	}
+	if (sum == origNum) {
+		cout << origNum << " - число Армстронга." << endl;
+	}
+	else
+		cout << origNum << " - не является числом Армстронга." << endl;
+	return 0;
+}
