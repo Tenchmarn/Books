0cd3766b86bc2cbfeb1cb0113ae0d3ae7031566e
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-07-02 16:21:29 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_2/28_example_(simple_calculator_using_switch_case).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	char op;
+	float num1, num2;
+	cout << "Введите оператор (+ - * /): ";
+	cin >> op;
+	cout << "Введите операнды: ";
+	cin >> num1 >> num2;
+
+	switch (op) {
+	case '+': {
+		cout << num1 + num2;
+		break;
+	}
+	case '-': {
+		cout << num1 - num2;
+		break;
+	}
+	case '*': {
+		cout << num1 * num2;
+		break;
+	}
+	case '/': {
+		if (num2 != 0) {
+			cout << num1 / num2;
+		}
+		else {
+			cout << "Divizion by  zero!";
+		}
+		break;
+	}
+	default: {
+		//			Если оператор отличается от (+ - * /)
+		cout << "Неправильный оператор!";
+	}
+	}
+
+	cout << endl;
+	return 0;
+}
