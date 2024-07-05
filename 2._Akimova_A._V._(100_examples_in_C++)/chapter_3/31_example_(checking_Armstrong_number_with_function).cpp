712e3f01de0d8ba2c1097bb94d2a53ee1ce42217
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-07-06 00:54:38 +0300

    Add files via upload

---
+++ 2._Akimova_A._V._(100_examples_in_C++)/chapter_3/31_example_(checking_Armstrong_number_with_function).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+int checkArmstrongNumber(int);
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int origNum;
+	cout << "Введите целое положительное число: ";
+	cin >> origNum;
+	if (checkArmstrongNumber(origNum) == 1)
+	{
+		cout << " - Число армстронга.";
+	}
+	else
+	{
+		cout << " - не является числом Армстронга.";
+	}
+	return 0;
+}
+int checkArmstrongNumber(int number) 
+{
+	int originalNumber, remainder, result = 0, n = 0, flag;
+	originalNumber = number;
+	while (originalNumber != 0)
+	{
+		originalNumber /= 10;
+		++n;
+	}
+	originalNumber = number;
+	while (originalNumber != 0) 
+	{
+		remainder = originalNumber % 10;
+		result += pow(remainder, n);
+		originalNumber /= 10;
+	}
+	//				Условие для числа Армстронга
+	if (result == number)
+	{
+		flag = 1;
+	}
+	else
+	{
+		flag = 0;
+	}
+	return flag;
+}
