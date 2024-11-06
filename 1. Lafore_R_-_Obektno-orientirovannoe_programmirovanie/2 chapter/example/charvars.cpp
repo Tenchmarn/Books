7ab94c96aaa8a558ade813d0fe20f2ff9a98b1a0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-06 22:04:24 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/charvars.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>				// для cout и т.п.
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	char charvar1 = 'A';		// символ переменная
+
+	char charvar2 = '\t';
+	cout << charvar1;
+	cout << charvar2;
+	charvar1 = 'B';
+
+	cout << charvar1;
+	cout << '\n';
+
+	return 0;
+}
