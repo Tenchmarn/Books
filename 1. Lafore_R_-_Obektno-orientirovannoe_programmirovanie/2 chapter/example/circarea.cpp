e1f0c000a8631d5ab1b579366b604deaaa791a79
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-06 22:04:58 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/circarea.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float rad;
+	const float PI = 3.14159F;
+	cout << "Введите радиус окружности: ";
+	cin >> rad;
+	float area = PI * rad * rad;
+	cout << "Площадь круга равна " << area << endl;
+	return 0;
+}
