3bcf33d6bc78e23b8e1e3dd36590488bbd107ada
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-07 12:07:25 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/fahren.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int ftemp;
+	cout << "Введите температуру по Фаренгейту: ";
+	cin >> ftemp;
+	int ctemp = (ftemp - 32) * 5 / 9;
+	cout << "Температура по Цельсию равна " << ctemp << '\n';
+	return 0;
+}
