e1c77ed916c120e65c946fc816add2a35beef01c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-07 23:54:43 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/mixed.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int count = 7;
+	float avgWeight = 155.5F;
+	double totalWeight = count * avgWeight;
+	cout << "Вес равен " << totalWeight << endl;
+	return 0;
+}
