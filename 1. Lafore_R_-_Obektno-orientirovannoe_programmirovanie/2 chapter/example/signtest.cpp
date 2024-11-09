eeed88ba5e7b69dc62c7f9fc013577b531bea9ab
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-09 03:14:32 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/signtest.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int signedVar = 1500000000;
+	unsigned int unsignVar = 1500000000;
+	signedVar = (signedVar * 2) / 3;
+	unsignVar = (unsignVar * 2) / 3;
+	cout << "Знаковая переменная равна " << signedVar << endl;
+	cout << "Беззнаковая переменная равна " << unsignVar << endl;
+	return 0;
+}
