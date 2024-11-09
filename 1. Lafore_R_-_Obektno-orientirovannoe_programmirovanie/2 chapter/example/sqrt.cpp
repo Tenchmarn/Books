251d9efdbd4abfd5dc7fa73452fe843e4f500562
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-09 03:14:47 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/sqrt.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double number, answer;
+	cout << "Введите число: ";
+	cin >> number;
+	answer = sqrt(number);
+	cout << "Квадратный корень равен "
+		<< answer << endl;
+	return 0;
+}
