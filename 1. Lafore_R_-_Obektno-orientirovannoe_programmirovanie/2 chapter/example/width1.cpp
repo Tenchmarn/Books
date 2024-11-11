f05ab45b6d54064c9f5680a9f45c281658639883
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-11 04:49:08 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/example/width1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	long pop1 = 4789426, pop2 = 274124, pop3 = 9761;
+	cout << "Город " << "Нас. " << endl
+		<< "Москва " << pop1 << endl
+		<< "Киров " << pop2 << endl
+		<< "Угрюмовка " << pop3 << endl;
+	return 0;
+}
