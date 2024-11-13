24ea9ede607c1333496bd9a8ed22b9cd2de60e72
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-13 21:46:43 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/exercises/5 exercises.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "ctype.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	char a;
+	cout << "Введите букву: ";
+	cin >> a;
+	cout << islower(a);
+	return 0;
+}
