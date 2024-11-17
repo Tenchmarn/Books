7218f3d81fb6af27a2ce9ec15fef730ae584eba0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-17 20:49:38 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/exercises/9 exercises.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	double a, b, c, d;
+	char dummychar;
+
+	cout << "Введите первую дробь: ";
+	cin >> a >> dummychar >> b;
+
+	cout << "Введите вторую дробь: ";
+	cin >> c >> dummychar >> d;
+
+	cout << "Сумма равна: " << (a * d + b * c) << dummychar << b*d<< endl;
+	
+
+	return 0;
+}
