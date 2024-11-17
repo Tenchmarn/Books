56c943a57bb2e8bf105f96e0e94465e9b6a34702
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-17 20:45:31 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/exercises/7 exercises.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	double temp1;
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите температуру в градусах по Цельсию: ";
+	cin >> temp1;
+	cout << "Температура по Фаренгейту: " << (temp1 - 32) / 1.8 << endl;
+	return 0;
+}
