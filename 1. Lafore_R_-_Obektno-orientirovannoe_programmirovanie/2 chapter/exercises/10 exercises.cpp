0de85bee5ab95afb1d7629ea5adca4683e6649dc
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-20 18:02:29 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/2 chapter/exercises/10 exercises.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char ch;
+	float funt, shiling, penc;
+
+	cout << "Введите количество фунтов: "; 
+	cin >> funt;
+	cout << "Введите количество шиллингов: ";
+	cin >> shiling;
+	cout << "Введите количество пенсов: ";
+	cin >> penc;
+
+	cout << "Десятичных фунтов: J" << funt << "." 
+		<< 1+static_cast<int>((shiling * 12 + penc) / 2.4);
+
+	return 0;
+}
