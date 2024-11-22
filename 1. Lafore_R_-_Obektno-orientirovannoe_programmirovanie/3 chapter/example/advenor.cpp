1275f279f4624378adc4393d1c1fbd9a97d79c5f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-22 15:41:54 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/advenor.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <process.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	char dir = 'a';
+	int x = 10, y = 10;
+
+	while (dir != '\r')
+	{
+		cout << "\nВаши координаты: " << x << ", " << y;
+
+		if (x < 5 || x>15)
+			cout << "\nОсторожно - дракон!";
+
+		cout << "\nВыберите направление (n, s, e, w): ";
+
+		dir = _getche();
+
+		switch (dir)
+		{
+		case 'n': y--; break;
+		case 's': y++; break;
+		case 'e': x++; break;
+		case 'w': x--; break;
+
+		}
+	}
+
+	return 0;
+}
