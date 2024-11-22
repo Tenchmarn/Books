50edb7cebef0e19feca0dd91f4a6790f6176051f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-22 15:42:12 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/badelse.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, b, c;
+	cout << "Введите числа a, b и c: \n";
+	cin >> a >> b >> c;
+	if (a == b)
+		if (b == c)
+			cout << "a, b и c равны\n";
+		else
+			cout << "a и b не равны\n";
+
+	return 0;
+}
