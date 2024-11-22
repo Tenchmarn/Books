8e136281bd1002258753086a849346b55aa26145
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-22 15:42:25 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/chcnt2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int chcount = 0;
+	int wdcount = 1;
+
+	char ch;
+
+	while ((ch = _getche()) != '\r')
+	{
+		if (ch == ' ')
+			wdcount++;
+		else
+			chcount++;
+	}
+
+	cout << "\nСлов: " << wdcount << endl;
+	cout << "Букв: " << chcount << endl;
+
+	return 0;
+}
