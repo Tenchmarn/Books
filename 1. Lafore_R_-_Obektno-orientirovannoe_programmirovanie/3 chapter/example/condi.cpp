67b1dbfacf3bb9bdca8238947481911d14b6d0f6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-11-25 16:47:10 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/condi.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	for (int j = 0; j < 80; j++)
+	{
+		char ch = (j % 8) ? ' ' : 'x';
+		cout << ch;
+	}
+
+
+	return 0;
+}
