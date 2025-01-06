33c94a0d7ea2e9d2b366eaf9a99a4cd0a58be028
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-01-06 16:22:05 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/factor.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	unsigned int numb;
+	unsigned long fact = 1;
+
+	cout << "Введите целове число: ";
+	cin >> numb;
+
+	for (int j = numb; j > 0; j--)
+		fact *= j;
+	cout << "Факториал числа равен " << fact << endl;
+
+	return 0;
+}
