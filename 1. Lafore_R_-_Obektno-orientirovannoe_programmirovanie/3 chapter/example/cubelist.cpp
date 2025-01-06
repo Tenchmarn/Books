364c526816fd9e22cee3b4f1e4f6da13ef47e741
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-01-06 16:20:58 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/cubelist.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int numb;
+	for (numb = 1; numb <= 10; numb++)
+	{
+		cout << setiosflags(ios::left) <<setw(4) << numb;
+		int cube = numb * numb * numb;
+		cout << setw(6) << cube << endl;
+	}
+
+	return 0;
+}
