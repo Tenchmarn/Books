da26289bb3a79037db52b04b1d1c03421797e03f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-01-06 16:21:31 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/divdo2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	long dividend, divisor;
+	char ch;
+	do 
+	{
+		cout << "Введите делимое: "; cin >> dividend;
+		cout << "Введите делитель: "; cin >> divisor;
+		if (divisor == 0)
+		{
+			cout << "Некорректный делитель!\n";
+			continue;
+		}
+
+		cout << "Частное равно " << dividend / divisor;
+		cout << ", остаток равен " << dividend % divisor;
+		cout << "\nЕще раз?(y/n): "; 
+		cin >> ch;
+	} while (ch != 'n');
+
+	return 0;
+}
