7de808612641b6cfb4874f11500413b954c9e869
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-16 00:50:46 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/platters.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int speed;
+
+	cout << "\nВведите число 33, 45 или 78: ";
+	cin >> speed;
+
+	switch (speed)
+	{
+	case 33:
+	{
+		cout << "Долгоиграющий формат\n";
+		break;
+	}
+	case 45:
+	{
+		cout << "Формат сигнал\n";
+	}
+	case 78:
+	{
+		cout << "Устаревший формат\n";
+	}
+	}
+
+	return 0;
+}
