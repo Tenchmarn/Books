38e79ffc77adeed9862422c2fc287fd00ccbc732
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-16 00:50:08 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/ifdemo.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x;
+	cout << "Введите чтисло: ";
+	cin >> x;
+
+	if (x > 100)
+		cout << "Это число больше, чем 100\n";
+
+	return 0;
+}
