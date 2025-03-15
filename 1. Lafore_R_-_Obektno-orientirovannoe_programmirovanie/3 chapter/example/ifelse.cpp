00c8b4fba36c449aae46221cf8f095c7902f413f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-16 00:50:24 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/ifelse.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int x;
+
+	cout << "\nВведите число: ";
+	cin >> x;
+
+	if (x > 100)
+		cout << "Это число больше, чем 100\n";
+	else
+		cout << "Это число не больше, чем 100\n";
+
+
+	return 0;
+}
