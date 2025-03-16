1e32fb1d88ebebb97b835835704af74c82177e77
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-16 21:50:10 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/relat.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int numb;
+
+	cout << "Введите число: ";
+	cin >> numb;
+
+	cout << "numb < 10 равно " << (numb < 10) << endl;
+	cout << "numb > 10 равно " << (numb > 10) << endl;
+	cout << "numb == 10 равно " << (numb == 10) << endl;
+
+	return 0;
+}
