bb43d55fe584ec3745723b0141ee85fc4e68d087
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-16 21:49:55 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/prime.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <process.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	unsigned long n, j;
+
+	cout << "Введите число: ";
+	cin >> n;
+
+	for (j = 2; j <= n / 2; j++)
+		if (n % j == 0)
+		{
+			cout << "Число не простое, делится на " << j << endl;
+			exit(0);
+		}
+	cout << "Число является простым\n";
+
+	return 0;
+}
