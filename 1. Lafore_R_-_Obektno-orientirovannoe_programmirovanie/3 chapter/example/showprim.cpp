0c6a24108946a927593ff01b3b4eade024c8f410
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-03-17 19:44:50 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/example/showprim.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	const unsigned char WHITE = 219; // белый цвет для простых чисел
+	const unsigned char GRAY = 176; // серый цвет для отсальных чисел
+	unsigned char ch;
+
+	for (int count = 0; count < 80 * 25 - 1; count++)
+	{
+		ch = WHITE;
+
+		for (int j = 2; j < count; j++)
+			if (count % j == 0)
+			{
+				ch = GRAY;
+				break;
+			}
+			cout << ch;
+	}
+	_getch();
+
+	return 0;
+}
