beec7a03f86f5487141b173fe0f5da4699aa7f6f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-29 09:15:55 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char ch = ' ';
+
+	long x_1 = 0;
+
+	cout << "Введите число: " ;
+
+	while ((ch = _getche()) != '\r')
+		x_1 = x_1 * 10 + ch - '0';
+
+	cout << "\nВы ввели число: " << x_1;
+
+
+	return 0;
+}
