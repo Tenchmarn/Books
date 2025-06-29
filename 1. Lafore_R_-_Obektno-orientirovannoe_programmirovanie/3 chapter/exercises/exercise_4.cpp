3529ab3a1f2e47d5870b1e623025e41158a3bd29
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-29 09:16:36 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char ch, ch_1;
+
+	float x_1, x_2, result = 0;
+
+	do
+	{
+		cout << "Введите первый операнд, операцию и второй операнд: ";
+		cin >> x_1 >> ch_1 >> x_2;
+		switch (ch_1) 
+		{
+		case '/': result = x_1 / x_2; break;
+		case '*': result = x_1 * x_2; break;
+		case '-': result = x_1 - x_2; break;
+		case '+': result = x_1 + x_2; break;
+			default:
+			{
+				cout << "Ошибка символа!" << endl;
+			}
+		}
+		cout << "Результат равен: " << result;
+		cout << "\nВыполнить еще одну операцию (y/n)? ";
+		cin >> ch;
+	} while (ch == 'y');
+
+	cout << endl;
+
+	return 0;
+}
