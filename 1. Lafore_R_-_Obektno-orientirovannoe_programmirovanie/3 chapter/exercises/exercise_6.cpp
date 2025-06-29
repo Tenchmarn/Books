7a901258db403c265842e57ae3cb4aed0f5a760f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-29 09:17:04 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	unsigned int numb;
+
+
+	do {
+
+		unsigned long fact = 1;
+
+		cout << "Введите целое число: "; cin >> numb;
+		for (int j = numb; j > 0; j--) 
+		{
+			fact *= j;
+		}
+
+		if (numb != 0)
+		{
+			cout << "Факториал числа равен " << fact << endl;
+		}
+
+	} while (numb != 0);
+
+	return 0;
+}
