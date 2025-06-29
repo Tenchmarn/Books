12dd591e847f238eef3dea73b2d57c26ec19dbc2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-06-29 09:16:51 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n = 20, p = 0;
+
+
+	int s = 2 * n - 1;
+
+	
+	for (int i = 1; i <= n; i++)
+	{
+		p = s - (2 * i - 1); 
+
+		for (int j = 1; j <= s; j++)
+		{
+			cout << ((j <= p) ? ' ' : 'x');
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
