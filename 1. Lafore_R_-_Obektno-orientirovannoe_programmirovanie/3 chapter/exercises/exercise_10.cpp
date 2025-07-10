23de3fc4a4b6230cd1bd2df99cafec299330ce44
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-10 12:45:12 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_10.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	double rate;       
+	double sum1, sum2; 
+	int years = 0;         
+
+	cout << "Введите ставку в процентах: "; cin >> rate;
+	cout << "Введите начальный вклад в рублях: "; cin >> sum1;
+	cout << "Введите итоговую сумму в рублях: "; cin >> sum2;
+
+	while (sum1 < sum2)
+	{
+		sum1 = sum1 + sum1 * rate / 100;
+		years++;
+	}
+
+	cout << "Вы получите " << sum1 << " руб. через " << years << " лет" << endl;
+
+
+	return 0;
+}
