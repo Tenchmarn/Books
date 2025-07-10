7c8a6192812e2b06884c3f3a667c493f7716c668
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-10 12:45:42 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_12.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, b, c, d;
+	char ch, oper;
+	int rez_znam = 0;
+	int rez_chisl = 0;
+
+	do 
+	{
+		cout << endl;
+
+		cout << "Введите первую дробь (a/b): ";
+		cin >> a >> ch >> b;
+
+		cout << "Введите вторую дробь (c/d): ";
+		cin >> c >> ch >> d;
+
+		cout << "Введите знак операции (+, -, *, /): ";
+		cin >> oper;
+
+		switch (oper)
+		{
+			case '+':
+			{
+				rez_znam = b * d;
+				rez_chisl = a * d + b * c;
+				break;
+			}
+			case '-':
+			{
+				rez_znam = b * d;
+				rez_chisl = a * d - b * c;
+				break;
+			}
+			case '*':
+			{
+				rez_znam = b * d;
+				rez_chisl = a * c;
+				break;
+			}
+			case '/':
+			{
+				rez_znam = b * c;
+				rez_chisl = a * d;
+				break;
+			}
+			default:
+			{
+				cout << "Ошибка!";
+			}
+		}
+
+		cout << "Вывод результата: " << rez_chisl << ch << rez_znam << endl;
+
+		cout << "\tЖелаете продолжить? (n/y): ";
+
+	} while ((ch = _getche()) != 'n');
+
+	cout << endl;
+
+	return 0;
+}
