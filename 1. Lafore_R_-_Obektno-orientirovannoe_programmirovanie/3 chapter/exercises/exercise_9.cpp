b14034c3d5550beff6225fb5952452909e730d0a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-10 12:44:52 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_9.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int guests, chairs;// число стульев, число гостей
+	int sum = 1;
+
+	cout << "Введите чилсо мест за столом: ";
+	cin >> guests;
+
+	cout << "Введите число гостей: ";
+	cin >> chairs;
+
+	while (chairs < guests)
+	{
+		cout << "Число гостей не должно быть меньше числа мест за столом!" << endl;
+		cout << "Введите число гостей: "; cin >> chairs;
+	}
+
+	for (int i = 1; i <= guests; i++) // перебор мест за столом
+	{
+		sum = sum * chairs;
+		chairs = chairs - 1;
+	}
+
+	cout << "Число рассадок гостей: " << sum << endl;
+
+	return 0;
+}
