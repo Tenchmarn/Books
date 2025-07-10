736b49d13a1f6819a5ae5183dd2f4fddf1498ce6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-10 12:44:13 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/3 chapter/exercises/exercise_7.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float contribution, bet; // вклад, ставка
+	int year;
+
+
+	cout << "Введите начальный вклад: ";
+	cin >> contribution;
+
+	cout << "Введите число лет: ";
+	cin >> year;
+
+	cout << "Введите процентную ставку: ";
+	cin >> bet;
+
+
+	for (int i = 1; i <= year; i++)
+	{
+		contribution += (contribution * (bet / 100));
+	}
+
+	cout << "Через " << year << " лет вы получите " << contribution << " доллара." << endl;
+
+
+	return 0;
+}
