ac4992f6247f0d0150bb6036e1cf608d07072304
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-14 13:17:30 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/example/dayenum.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum days_of_week {Sun, Mon, Tue, Wed, Thu, Fri, Sat};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	days_of_week day_1, day_2;
+
+	day_1 = Mon;
+	day_2 = Thu;
+
+	int diff = day_2 - day_1;
+
+	cout << "Разница в днях: " << diff << endl;
+
+	if (day_1 < day_2)
+		cout << "day_1 наступит раньше, чем day_2\n";
+
+	return 0;
+}
