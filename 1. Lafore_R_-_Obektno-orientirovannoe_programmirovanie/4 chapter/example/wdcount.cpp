672f5fb9d3dcc7825efa970da65cc7affa1400f7
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-17 08:40:06 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/example/wdcount.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+enum itsaWord {NO, YES};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);			// функция задает входную кодовую страницу
+    SetConsoleOutputCP(1251);	// Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим
+
+
+	itsaWord isWord = NO;
+
+	char ch = 'a';
+
+	int wordcount = 0;
+
+	cout << "Введите предложение: \n";
+
+	do {
+		ch = _getche();
+
+        if (ch == ' ' || ch == '\r')// если введен пробел, 
+        {
+            if (isWord == YES)        // а до этого вводилось слово, 
+            {                      // значит, слово закончилось 
+                wordcount++;           // учет слова 
+                isWord = NO;           // сброс флага 
+            }
+        }                        // в противном случае 
+        else                       // ввод слова продолжается 
+            if (isWord == NO)         // если начался ввод слова, 
+                isWord = YES;          // то устанавливаем флаг 
+    } while (ch != '\r');        // выход по нажатию Enter 
+
+    cout << "\n---Число слов: " << wordcount << "---\n";
+
+    return 0;
+}
