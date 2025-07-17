5f6e4ed1016f34c79427e8fdd57a790ee6302c67
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-17 08:39:04 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/example/parts.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct part
+{
+	int modelnumber;
+	int partnumber;
+	float cost;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	part part1;
+
+	part1.modelnumber = 6244;
+	part1.partnumber = 373;
+	part1.cost = 217.55F;
+
+	cout << "Модель " << part1.modelnumber;
+	cout << ", деталь " << part1.partnumber;
+	cout << ", стоимость $" << part1.cost << endl;
+
+	return 0;
+}
