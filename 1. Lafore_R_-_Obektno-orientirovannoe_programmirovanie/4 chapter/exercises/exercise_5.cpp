309af3296a1cd6dc92f6b9825b67f3fdc9d8296d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-17 08:41:21 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/exercises/exercise_5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct date
+{
+	int day;
+	int month;
+	int year;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	struct date date_1;
+	char ch = ' ';
+
+	cout << "Введите день, месяц и год в формате (дд/мм/гггг): ";
+	cin >> date_1.day >> ch >> date_1.month >> ch >> date_1.year;
+
+	cout << "Вы ввели дату: "
+		<< date_1.day << ch << date_1.month << ch << date_1.year;
+
+	cout << endl;
+
+
+
+	return 0;
+}
