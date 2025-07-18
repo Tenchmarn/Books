b17c8e979058cef04373b9ad52bc497c15e0a11d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-18 13:42:13 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/exercises/exercise_8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct fraction
+{
+	int chislitel;
+	int znamenatel;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	struct fraction d1, d2;
+
+	char h1;
+
+	cout << "Введите первую дробь: "; 
+	cin >> d1.chislitel >> h1 >> d1.znamenatel;
+
+	cout << "Введите вторую дробь: "; 
+	cin >> d2.chislitel >> h1 >> d2.znamenatel;
+	cout << "Сумма равна: " 
+		<< (d1.chislitel * d2.znamenatel + d1.znamenatel * d2.chislitel)
+		<< h1 
+		<< d1.znamenatel * d2.znamenatel
+		<< endl;
+
+	return 0;
+}
