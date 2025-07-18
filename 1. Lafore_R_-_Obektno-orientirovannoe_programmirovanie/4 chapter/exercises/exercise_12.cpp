2ae65193c489e79a86da761b93c01f4f9837479f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-18 13:43:39 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/exercises/exercise_12.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct fraction
+{
+	int chislitel;
+	int znamenatel;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	fraction d1, d2;
+
+	char ch, oper;
+	int rez_znam = 0;
+	int rez_chisl = 0;
+
+	do
+	{
+		cout << endl;
+
+		cout << "Введите первую дробь (a/b): ";
+		cin >> d1.chislitel >> ch >> d1.znamenatel;
+
+		cout << "Введите вторую дробь (c/d): ";
+		cin >> d2.chislitel >> ch >> d2.znamenatel;
+
+		cout << "Введите знак операции (+, -, *, /): ";
+		cin >> oper;
+
+		switch (oper)
+		{
+		case '+':
+		{
+			rez_znam = d1.znamenatel * d2.znamenatel;
+			rez_chisl = d1.chislitel * d2.znamenatel + d1.znamenatel * d2.chislitel;
+			break;
+		}
+		case '-':
+		{
+			rez_znam = d1.znamenatel * d2.znamenatel;
+			rez_chisl = d1.chislitel * d2.znamenatel - d1.znamenatel * d2.chislitel;
+			break;
+		}
+		case '*':
+		{
+			rez_znam = d1.znamenatel * d2.znamenatel;
+			rez_chisl = d1.chislitel * d2.chislitel;
+			break;
+		}
+		case '/':
+		{
+			rez_znam = d1.znamenatel * d2.chislitel;
+			rez_chisl = d1.chislitel * d2.znamenatel;
+			break;
+		}
+		default:
+		{
+			cout << "Ошибка!";
+		}
+		}
+
+		cout << "Вывод результата: " << rez_chisl << ch << rez_znam << endl;
+
+		cout << "\tЖелаете продолжить? (n/y): ";
+
+	} while ((ch = _getche()) != 'n');
+
+	cout << endl;
+
+	return 0;
+}
