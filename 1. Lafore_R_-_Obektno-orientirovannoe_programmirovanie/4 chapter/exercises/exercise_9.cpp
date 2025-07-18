bfcc2f78bae3fc4184ffa8743f05ee1361bb3b7a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-18 13:42:32 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/exercises/exercise_9.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct time
+{
+	int hours;
+	int minutes;
+	int seconds;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	struct time t1;
+	char ch;
+
+	cout << "Введите время в формате (чч.мм.сс): ";
+	cin >> t1.hours >> ch >> t1.minutes >> ch >> t1.seconds;
+
+	long totalseсs = t1.hours * 3600 + t1.minutes * 60 + t1.seconds;
+
+	cout << "Секундный формат времени: " << totalseсs;
+
+	cout << endl;
+
+	return 0;
+}
+
