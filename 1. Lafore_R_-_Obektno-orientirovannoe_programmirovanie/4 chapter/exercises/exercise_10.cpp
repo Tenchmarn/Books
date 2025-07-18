202d25dea879ba6aae2442e3a3ee4215817320a4
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-18 13:42:57 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/4 chapter/exercises/exercise_10.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct sterling
+{
+	int pounds;
+	int shillings;
+	int pences;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	struct sterling old;
+
+	double decpounds;
+
+	cout << "Введите денежную сумму в десятичных фунтах: ";
+	cin >> decpounds;
+
+	old.pounds = decpounds;
+	old.shillings = (decpounds - old.pounds) * 20;
+	old.pences = ((decpounds - old.pounds) * 20 - old.shillings) * 12;
+
+	cout << "В старой английской системе составит: "
+		<< old.pounds << " фунтов, "
+		<< old.shillings << " шиллингов и "
+		<< old.pences << endl;
+
+
+	return 0;
+}
