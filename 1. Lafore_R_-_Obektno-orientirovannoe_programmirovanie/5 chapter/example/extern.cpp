7b54246e607437b53c1be7eb6fe0cba7334c5a5d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-19 22:28:13 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/extern.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#include <conio.h>
+
+char ch = 'a';
+
+void getachar();
+void putachar();
+
+int main()
+{
+	while (ch != '\r')
+	{
+		getachar();
+		putachar();
+	}
+	cout << endl;
+
+	return 0;
+}
+
+void getachar()
+{
+	ch = _getch();
+}
+
+void putachar()
+{
+	cout << ch;
+}
+
