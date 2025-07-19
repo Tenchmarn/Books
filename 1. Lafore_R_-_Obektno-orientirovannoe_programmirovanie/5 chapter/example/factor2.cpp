84f5356def035e654d984f07f4141c00a2d95f51
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-19 22:28:34 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/factor2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+unsigned long factfunc(unsigned long);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	unsigned long fact;
+
+	cout << "Введите целое число: ";
+	cin >> n;
+
+	fact = factfunc(n);			// 5
+
+	cout << "Факториал числа " << n << " равен = " << fact << endl;
+
+	return 0;
+}
+
+
+unsigned long factfunc(unsigned long n)			// 5
+{
+	if (n > 1)
+	{
+		return n * factfunc(n - 1);
+	}
+	else
+		return 1;
+}
