ea00b3547f212411b13b9eb41def596fb14e49a2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-23 01:55:41 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/inliner.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+inline float lbstokg(float pounds)
+{
+	return 0.453592 * pounds;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float lbs;
+
+	cout << "\nВведите вес в фунтах: ";
+	cin >> lbs;
+
+	cout << " " << lbstokg(lbs) << endl;
+
+	return 0;
+}
