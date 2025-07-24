f2f5dd0e3184f86708f8cde416c3e8f35737bf27
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-24 08:45:50 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/overload.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void repchar();
+void repchar(char);
+void repchar(char, int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	repchar();
+	repchar('=');
+	repchar('+', 100);
+
+	return 0;
+}
+
+
+void repchar()
+{
+	for (int j = 0; j < 45; j++)
+	{
+		cout << '*';
+	}
+	cout << endl;
+}
+
+void repchar(char ch)
+{
+	for (int j = 0; j < 45; j++)
+	{
+		cout << ch;
+	}
+	cout << endl;
+}
+
+
+void repchar(char ch, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cout << ch;
+	}
+	cout << endl;
+}
