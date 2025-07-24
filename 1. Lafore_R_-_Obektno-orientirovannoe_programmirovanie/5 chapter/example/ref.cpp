73e7180c10a4c990650abd412bada52e986693ab
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-24 08:46:06 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/ref.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	void intfrac(float, float&, float&);
+	float number, intpart, fracpart;
+
+	do {
+		cout << "\nВведите вещественное число: ";
+		cin >> number;
+
+		intfrac(number, intpart, fracpart); 
+
+		cout << "Целая часть равна " << intpart
+			<< ", дробна  часть равна " << fracpart << endl;
+	} while (number != 0.0);
+
+	return 0;
+}
+
+void intfrac(float n, float& intp, float& fracp)
+{
+	long temp = static_cast<long>(n);
+
+	intp = static_cast<float>(temp);
+	fracp = n - intp;
+}
