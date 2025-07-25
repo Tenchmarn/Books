39b3061daf3b2c53f6b1cd32e04bcd4a5bca5154
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-25 08:38:00 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/retref.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int x;
+
+int& setx();
+
+int main()
+{
+	setx() = 92;
+	cout << "x = " << x << endl;
+	return 0;
+}
+
+int& setx()
+{
+	return x;
+}
