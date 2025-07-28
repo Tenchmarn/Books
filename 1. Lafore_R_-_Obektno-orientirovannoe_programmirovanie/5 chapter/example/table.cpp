e0936b839bc43eb3e4cdc18aae2dcb26ee8642e1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-29 00:09:07 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/table.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void starline();
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	starline();
+	cout << "Тип данных Диапазон" << endl;
+	cout << "char   -128...127" << endl;
+	cout << "int    Системно-зависимый" << endl;
+	cout << "long   -2 147 483 648... 2 147 483 647" << endl;
+
+	starline();
+
+
+	return 0;
+}
+
+void starline()
+{
+	for (int j = 0; j < 45; j++)
+	{
+		cout << '*';
+	}
+	cout << endl;
+}
