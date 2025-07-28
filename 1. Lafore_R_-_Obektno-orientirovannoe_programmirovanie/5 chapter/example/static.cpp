35dc39587339b85fa83f7ad2047b8c8f1ea5cf15
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-07-29 00:08:44 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/static.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float getavg(float);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	float data = 1, avg;
+
+	while (data != 0)
+	{
+		cout << "Введите число: ";
+		cin >> data;
+
+		avg = getavg(data);
+		cout << "Среднее значение: " << avg << endl;
+	}
+
+	return 0;
+}
+
+float getavg(float newdata)
+{
+	static float total = 0;
+	static int count = 0;
+	count++;
+	total += newdata;
+	return total / count;
+}
+
