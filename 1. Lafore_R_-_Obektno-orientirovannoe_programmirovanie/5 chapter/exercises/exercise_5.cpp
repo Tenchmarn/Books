b383a6cf7dfac0dca8da5efe13a371781b723cac
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-03 22:32:43 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/exercises/exercise_5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+inline long hms_to_secs(int&, int&, int&);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int hour, minut, second;
+	char ch = ' ';
+
+	do {
+
+		cout << "Введите часы: ";
+		cin >> hour;
+
+		cout << "Введите минуты: ";
+		cin >> minut;
+
+		cout << "Введите секунды: ";
+		cin >> second;
+
+		cout << "Результат в секундах: "
+			<< hms_to_secs(hour, minut, second) << endl;
+
+		cout << "Желаете продолжить?(n/y): ";
+		cin >> ch;
+	} while (ch == 'y');
+
+	return 0;
+}
+
+inline long hms_to_secs(int& h, int& mi, int& sec)
+{
+	return sec + (mi * 60) + (h * 3600);
+}
