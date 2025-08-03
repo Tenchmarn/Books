3d195726982f292020bf3e4a47000fe50be0fa82
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-03 22:32:30 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/exercises/exercise_4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Distance
+{
+	int feet;
+	float inches;
+};
+
+void maxSmaller(Distance&, Distance&);
+void engldisp(Distance dd);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	Distance d1, d2;
+
+	cout << "\nВведите число футов: "; cin >> d1.feet;
+	cout << "Введите число дюймов: "; cin >> d1.inches;
+
+	cout << "\nВведите число футов: "; cin >> d2.feet;
+	cout << "Введите число дюймов: "; cin >> d2.inches;
+
+	cout << "Результат. " << '\n';
+
+	maxSmaller(d1, d2);
+
+	return 0;
+}
+
+void engldisp(Distance dd)
+{
+	cout << dd.feet << "\'-" << dd.inches << "\"";
+}
+
+void maxSmaller(Distance& n1, Distance& n2)
+{
+	int sum1 = ((n1.feet * 12) + n1.inches);
+	int sum2 = ((n2.feet * 12) + n2.inches);
+	if (sum1 > sum2)
+	{
+		cout << "n1 = ";
+		engldisp(n1);
+	}
+	else if (sum1 == sum2)
+	{
+		cout << "\tравны";
+		cout << endl;
+	}
+	else
+	{
+		cout << "n2 = ";
+		engldisp(n2);
+	}
+}
