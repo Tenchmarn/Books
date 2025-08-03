2f7473abc93b1f9f0d584a0be818f4af6cc5867f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-03 22:31:47 +0300

    vararg.cpp

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/example/vararg.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void repchar(char, int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char chin;
+	int nin;
+
+	cout << "Введите символ: ";
+	cin >> chin;
+
+	cout << "Введите число повторений символа: ";
+	cin >> nin;
+
+	repchar(chin, nin);
+
+	return 0;
+}
+
+void repchar(char ch, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		cout << ch;
+	}
+	cout << endl;
+}
+
