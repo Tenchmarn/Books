15c1bdd0b76d106670027c2a8cdff669f30011a6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-04 08:11:36 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/5 chapter/exercises/exercise_10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int i = 0;
+
+void count_me();
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	for (int j = 0; j < 10; j++)
+	{
+		count_me();
+	}
+
+	cout << i;
+
+	return 0;
+}
+
+void count_me()
+{
+	i++;
+}
