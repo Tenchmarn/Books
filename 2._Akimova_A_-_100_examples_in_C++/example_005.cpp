b72dc885b880994e71be9688cadd6d09d606605c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-09 13:34:44 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_005.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Размер char: " << sizeof(char) << " byte" << endl;
+	cout << "Размер int: " << sizeof(int) << " byte" << endl;
+	cout << "Размер float: " << sizeof(float) << " byte" << endl;
+	cout << "Размер double: " << sizeof(double) << " byte" << endl;
+
+	return 0;
+}
