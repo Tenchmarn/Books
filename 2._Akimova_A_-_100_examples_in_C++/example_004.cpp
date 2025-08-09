a764ae8a357c336172a2bff885cb0296f8b31e1c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-09 13:34:32 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_004.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int divisor, dividend, quotient, remainder;
+
+	cout << "Введите делимое: ";
+	cin >> dividend;
+
+	cout << "Введите делитель: ";
+	cin >> divisor;
+
+	quotient = dividend / divisor;
+	remainder = dividend % divisor;
+
+	cout << "Частное = " << quotient << endl;
+	cout << "Остаток = " << remainder << endl;
+
+
+	return 0;
+}
