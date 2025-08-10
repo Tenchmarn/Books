7e42cec24e589f6f5dc26decd9a591b1bc59c3c7
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-10 11:58:54 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/6 chapter/example/ecopycon.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Distance
+{
+private:
+	int feet;
+	float inches;
+public:
+	Distance() : feet(0), inches(0.0f)
+	{
+
+	}
+
+	Distance(int ft, float in) : feet(ft), inches(in)
+	{
+
+	}
+
+	void getdist()
+	{
+		cout << "\nВведите число футов: "; cin >> feet;
+		cout << "Введите число дюймов: "; cin >> inches;
+	}
+
+	void showdis()
+	{
+		cout << feet << "\'" << inches << '\"';
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	Distance dist1(11, 6.25f);
+	Distance dist2(dist1);
+
+	Distance dist3 = dist1;
+
+	cout << "\ndist1 = "; dist1.showdis();
+	cout << "\ndist2 = "; dist2.showdis();
+	cout << "\ndist3 = "; dist3.showdis();
+
+	cout << endl;
+
+	return 0;
+}
