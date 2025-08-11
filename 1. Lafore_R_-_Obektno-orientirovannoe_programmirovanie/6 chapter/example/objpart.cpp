08cb6943def866e2d7a38689d97876942984f797
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-11 08:18:50 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/6 chapter/example/objpart.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class part
+{
+private:
+	int modelnumber;
+	int partnumber;
+	float cost;
+public:
+	void setpart(int mn, int pn, float c)
+	{
+		modelnumber = mn;
+		partnumber = pn;
+		cost = c;
+	}
+
+	void showpart()
+	{
+		cout << "Модель " << modelnumber;
+		cout << ", деталь " << partnumber;
+		cout << ", стоимость $" << cost << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	part part1;
+
+	part1.setpart(6244, 373, 217.55f);
+	part1.showpart();
+
+	return 0;
+}
