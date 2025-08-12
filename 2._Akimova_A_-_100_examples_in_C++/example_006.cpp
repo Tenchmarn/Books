f899fb16b0d56c08c801a4ca2fbc7fa58b227472
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-12 08:27:26 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_006.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	// Вариант 1
+
+	int a = 1, b = 2;
+
+	cout << "До замены: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+
+	a = a + b; // a = 1+2 = 3
+	b = a - b; // b = 3-2 = 1
+	a = a - b; // a = 3-1 = 2
+
+	cout << "\nПосле замены: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+
+
+	return 0;
+}
+
+
+// Вариант 2
+
+/*
+	int a = 5, b = 10, temp;
+
+	cout << "До замены: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+
+	temp = a;
+	a = b;
+	b = temp;
+
+	cout << "\nПосле замены: " << endl;
+	cout << "a = " << a << ", b = " << b << endl;
+*/
+
