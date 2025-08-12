2828d1bc55ef36e9ecebcad794b95c19b269d987
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-12 08:26:45 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/6 chapter/example/statdata.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class foo
+{
+private:
+
+	static int count;
+
+public:
+
+	foo()
+	{
+		count++;
+	}
+
+	int getcount()
+	{
+		return count;
+	}
+};
+
+int foo::count = 0;		// опредление count
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	foo f1, f2, f3;
+
+	cout << "Число объектов: " << f1.getcount() << endl;
+	cout << "Число объектов: " << f2.getcount() << endl;
+	cout << "Число объектов: " << f3.getcount() << endl;
+
+
+	return 0;
+}
