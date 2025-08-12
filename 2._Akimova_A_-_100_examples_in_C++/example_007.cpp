bae619a3a3ca998c29df3bda044b14ac570592e8
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-12 08:27:49 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_007.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char c;
+
+	cout << "Введите символ: ";
+	cin >> c;
+	
+	cout << "ASCII - значение: " << int(c) << endl;
+
+	return 0;
+}
