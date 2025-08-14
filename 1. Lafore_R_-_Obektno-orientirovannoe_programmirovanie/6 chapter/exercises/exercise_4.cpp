b043f231d7c9e3e2d364c7be394808c736d2fb9b
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-15 00:23:22 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/6 chapter/exercises/exercise_4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class employee
+{
+private:
+	int number;
+	float stipend;
+public:
+	employee():number(0), stipend(0.0f) { }
+	void display() const;
+	void getemp();
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	employee employee_1;
+	employee employee_2;
+	employee employee_3;
+
+	employee_1.getemp();
+	employee_2.getemp();
+	employee_3.getemp();
+
+	employee_1.display();
+	employee_2.display();
+	employee_3.display();
+
+	cout << endl;
+
+	return 0;
+}
+
+void employee::display() const
+{
+	cout << "\n\tНомер сотрудника (" << number
+		<< "), пособие $" << stipend;
+}
+
+void employee::getemp()
+{
+	cout << "\nВведите номер сотрудника: "; cin >> number;
+	cout << "Введите величину оклада сотрудника: "; cin >> stipend;
+}
