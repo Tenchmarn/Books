5a244daf9c6d141745bfec09867b6bdaad8b76e5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-15 08:43:28 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_008.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	double first, second, product;
+
+	cout << "Введите два числа: "; 
+	cin >> first >> second;
+
+	product = first * second;
+
+	cout << "Результат = " << product << endl;
+
+	return 0;
+}
