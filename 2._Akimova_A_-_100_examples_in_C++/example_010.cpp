6a626b2fba550d3a7be2a7f79e21e514dc2bdb39
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-15 08:44:51 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_010.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	
+	// 1 вариант
+	int a, b, c, max;
+
+	cout << "Введите три целых числа: ";
+	cin >> a >> b >> c;
+
+	if (a >= b && a >= c)
+		max = a;
+	if (b >= a && b >= c)
+		max = b;
+	if (c >= a && c >= b)
+		max = c;
+
+	cout << "Мах: " << max;
+	cout << '\n';
+
+	// 2 вариант
+	/*
+	* float n1, n2, n3;
+	* 
+	* cout << "Введите три целых числа: ";
+	* cin >> n1 >> n2 >> n3;
+	* 
+	* if((n1 >= n2) && (n1 >= n3))
+	*	cout << "Максимум: " << n1;
+	* else if ((n2 >= n1) && (n2 >= n3))
+	*	cout << "Максимум: " << n2;
+	* else 
+	*	cout << "Максимум: " << n3;
+	* 
+	* cout << endl
+	*/
+
+
+	return 0;
+}
