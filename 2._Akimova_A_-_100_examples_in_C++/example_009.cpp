03da152046640d64b7b6e68da164f7549c51fea2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-15 08:44:01 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_009.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	cout << "Введите число: ";
+	cin >> n;
+
+	if (n % 2 == 0)
+		cout << n << " - четное.";
+	else
+		cout << n << " - нечетное.";
+
+	cout << endl;
+
+
+	return 0;
+}
