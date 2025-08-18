968e4548673e0c3f6e1bdab271a3573314952f2d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-18 07:54:58 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/6 chapter/exercises/exercise_8.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class objekt_counter
+{
+private:
+	unsigned int number;
+	static int count;
+public:
+	objekt_counter() 
+	{
+		count++; number = count;
+	}
+	void displayNumber() const;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	objekt_counter c1, c2, c3;
+
+	c1.displayNumber();
+
+	c2.displayNumber();
+
+	c3.displayNumber();
+
+	c2.displayNumber();
+
+	cout << endl;
+
+	return 0;
+}
+
+int objekt_counter::count = 0;
+
+void objekt_counter::displayNumber() const
+{
+	cout << "Мой порядковый номер: " << number << endl;
+}
