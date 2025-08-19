7ad418facf20f358291df584f0daf18397da44f5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 08:46:09 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_017.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+			// б;
+
+	int n1, n2;
+
+	cout << "Введите два числа: ";
+	cin >> n1 >> n2;
+
+	while (n1 != n2)
+	{
+		if (n1 > n2)
+			n1 -= n2;
+		else
+			n2 -= n1;
+	}
+
+	cout << "НОД = " << n1 << endl;
+
+	return 0;
+}
+
+			// a;
+
+/*
+
+int n1, n2, hcf = 0;
+
+cout << "Введите два числа: ";
+cin >> n1 >> n2;
+
+if (n2 > n1)
+{
+	int temp = n2;
+	n2 = n1;
+	n1 = temp;
+}
+
+for (int i = 1; i <= n2; ++i)
+{
+	if (n1 % i == 0 && n2 % i == 0)
+	{
+		hcf = i;
+	}
+}
+
+cout << "НОД = " << hcf << endl;
+
+*/
