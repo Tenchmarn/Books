98203dc6f864164072a6631f158c5273b7bdeed6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 08:35:01 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_014.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	unsigned int n;
+	unsigned long long factorial = 1;
+
+	cout << "Введите целое положительное число: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		factorial *= i;
+	}
+
+	cout << "\nФакториал " << n << " = " << factorial << endl;
+
+
+	return 0;
+}
