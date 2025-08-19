b8097433c171920aaab211612c0584e605b5b22a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 08:35:14 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_015.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i, j;
+
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			cout.width(2);
+			cout << i * j << ' ';
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
