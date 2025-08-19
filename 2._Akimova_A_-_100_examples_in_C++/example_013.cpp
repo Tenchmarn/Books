0d2e82f4c1e2de785cddf096de354c8cdbbff028
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 08:34:47 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_013.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+							// Вариант 1
+
+	int n, sum = 0;
+
+	cout << "Введите положительное целое число: "; 
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		sum += i;
+	}
+
+	cout << "Сумма = " << sum << endl;
+
+
+							// Вариант 2
+
+	/*
+	* int n, i, sum = 0;
+	* cout << "Введите целое число ";
+	* cin >> n;
+	* 
+	* i = 1;
+	* while(i <=n)
+	* {
+	*	sum += i;
+	*	++i;
+	* }
+	* cout << "\nСумма = " << sum << endl;
+	*/
+
+							// Вариант 3
+
+	/*
+	* int n, i, sum = 0;
+	* 
+	* do
+	* {
+	*	cout << "Введите целое положительное число: ";
+	*	cin >> n;
+	* } 
+	* while(n <= 0)
+	* 
+	* for(i = 1; i <= n; ++i)
+	* {
+	*	sum += i;
+	* }
+	* 
+	* cout << "\nСумма = " << sum << endl;
+	*/
+
+	return 0;
+}
