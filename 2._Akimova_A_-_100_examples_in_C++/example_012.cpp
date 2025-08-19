7de4df8c0b08e2a650c126d1c79187d11eaf481a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-19 08:34:29 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_012.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int year;
+
+	cout << "Введите год: "; cin >> year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				cout << year << " - высокосный";
+			}
+			else
+			{
+				cout << year << " - невысокосный";
+			}
+		}
+		else
+		{
+			cout << year << " - высокосный";
+		}
+	}
+	else
+	{
+		cout << year << " - невысокосный";
+	}
+
+	cout << endl;
+	return 0;
+}
