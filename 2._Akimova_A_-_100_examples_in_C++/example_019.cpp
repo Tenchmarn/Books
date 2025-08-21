3a05b446ad16d6122925da417a8b1522da83a5eb
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-21 08:50:05 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_019.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	long long n;
+
+	int count = 0;
+
+	cout << "Введите целое число: ";
+	cin >> n;
+
+	while (n != 0)
+	{
+		n /= 10;
+		++count;
+	}
+
+	cout << "Количество разрядов: " << count << endl;
+
+	return 0;
+}
