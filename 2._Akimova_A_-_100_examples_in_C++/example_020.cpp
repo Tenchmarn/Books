c30171a60f3ab699bdf24025ea581b720fecb299
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-26 08:26:05 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_020.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, reversedNumber = 0, remainder;
+
+	cout << "Введите целое число: ";
+	cin >> n;
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversedNumber = reversedNumber * 10 + remainder;
+		n /= 10;
+	}
+
+	cout << "Обратное число: " << reversedNumber << endl;
+
+	return 0;
+}
