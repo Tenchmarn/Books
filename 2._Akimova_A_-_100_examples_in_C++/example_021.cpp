51d49bc48334044739445c1db0a68859f4685683
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-29 08:26:28 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_021.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int exponent;
+	float base, result = 1;
+
+	cout << "Введите число и степень соответственно: ";
+	cin >> base >> exponent;
+
+	cout << base << "^" << exponent << " = ";
+
+	while (exponent != 0)
+	{
+		result *= base;
+		--exponent;
+	}
+	cout << result << endl;
+
+	return 0;
+}
