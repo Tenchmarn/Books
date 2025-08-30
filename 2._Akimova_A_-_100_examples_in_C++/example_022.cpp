83c94044e6514ddd8f313371b0f9bd5c4bd207a1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-08-31 00:28:45 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_022.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, reversed = 0, remainder, original;
+
+	cout << "Введите целое число: ";
+	cin >> n;
+
+	original = n;
+
+	// вычисляем обратное число
+	while (n!=0)
+	{
+		remainder = n % 10;
+		reversed = reversed * 10 + remainder;
+		n /= 10;
+	}
+
+	// палиндром, если исходное число и обратное одинаковые
+	if (original == reversed)
+	{
+		cout << original << " - палиндром\n";
+	}
+	else
+	{
+		cout << original << " - не палиндром\n";
+	}
+
+	return 0;
+}
