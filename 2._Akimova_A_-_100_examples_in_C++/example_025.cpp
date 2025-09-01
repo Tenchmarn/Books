ff09eaeb627820744fd51865ced25d657b33bafe
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-01 08:42:53 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_025.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int origNum, num, digit, sum = 0;
+
+	cout << "Введите целове положительное число: ";
+
+	cin >> origNum;
+
+	num = origNum;
+
+	while (num != 0)
+	{
+		digit = num % 10;
+		sum += digit * digit * digit;
+		num /= 10;
+	}
+
+	if (sum == origNum)
+	{
+		cout << origNum << " - число Армстронга." << endl;
+	}
+	else
+	{
+		cout << origNum << " - не число Армстронга." << endl;
+	}
+
+	return 0;
+}
