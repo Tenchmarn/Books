b09ee6ea9c1904703183ab98d613c4f2c2e4efca
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-01 08:42:37 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_024.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int low, high, i, flag;
+
+	cout << "Выводим простые числа между a и b, введите a и b: ";
+	cin >> low >> high;
+
+	while (low < high)
+	{
+		flag = 0;
+
+		for (i = 2; i <= low / 2; ++i)
+		{
+			if (low % 2 == 0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+		{
+			cout << low << " ";
+		}
+		++low;
+	}
+
+	cout << endl;
+
+	return 0;
+}
