6ba952e91a22dcea07ef66278b9dce9f26f2b9b2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-03 08:42:13 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_026.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
+
+	cout << "Введите начало и конец диапазона: ";
+	cin >> low >> high;
+
+
+	for (i = low + 1; i < high; ++i)
+	{
+		temp2 = i;
+		temp1 = i;
+
+		// кол-во разрядов
+		while (temp1 != 0)
+		{
+			temp1 /= 10;
+			++n;
+		}
+
+		// результат содержит сумму цифр, возведенных в степень n;
+		while (temp2 != 0)
+		{
+			remainder = temp2 % 10;
+			result += pow(remainder, n);
+			temp2 /= 10;
+		}
+
+		// проверяем, является ли число суммой n степени своих цифр
+		if (result == i)
+		{
+			cout << i << " ";
+		}
+
+		// сброс значений перед следующей итерацией 
+		n = 0;
+		result = 0;
+	}
+
+	cout << '\n';
+
+	return 0;
+}
