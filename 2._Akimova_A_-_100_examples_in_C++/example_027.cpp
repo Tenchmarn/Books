b61001d784a2d9cd060992589d554c412cdce8a2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-04 08:39:24 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_027.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int rows;
+
+
+	cout << "Введите количество строк: ";
+	cin >> rows;
+
+	for (int i = 1; i <= rows; ++i)
+	{
+		for (int j = 1; j <= i; ++j)
+		{
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+
+
+	return 0;
+}
