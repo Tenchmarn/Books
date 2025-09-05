d633cb0b8504df8edbafa3bc67f8a4b4d8bc4334
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-05 08:39:34 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_028.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char op;
+	float num1, num2;
+
+	cout << "Введите оператор (+ - * /): ";
+	cin >> op;
+
+	cout << "Введите операнды: ";
+	cin >> num1 >> num2;
+
+	switch (op)
+	{
+		case '+': cout << num1 + num2; break;
+		case '-': cout << num1 - num2; break;
+		case '*': cout << num1 * num2; break;
+		case '/': cout << num1 / num2; break;
+		default: 
+		{
+			// если оператор отличается
+			cout << "Неправильный оператор!";
+			break;
+		}
+	}
+
+	cout << endl;
+
+	return 0;
+}
