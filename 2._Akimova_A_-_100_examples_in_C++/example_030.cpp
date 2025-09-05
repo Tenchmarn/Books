0a6049ff552c782a436aae017e813f0dc3f2ccd6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-05 08:39:57 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_030.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int checkPrimeNumber(int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	cout << "Введите положительное число: ";
+	cin >> n;
+
+	if (checkPrimeNumber(n) == true)
+	{
+		cout << n << " - это простое число";
+	}
+	else
+	{
+		cout << n << " - это не простое число";
+	}
+
+	cout << endl;
+
+
+	return 0;
+}
+
+int checkPrimeNumber(int n)
+{
+	bool flag = true;
+
+	for (int i = 2; i <= n / 2; ++i)
+	{
+		if (n % i == 0)
+		{
+			flag = false;
+			break;
+		}
+	}
+
+	return flag;
+}
