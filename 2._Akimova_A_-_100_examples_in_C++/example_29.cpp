f8233fc56396a93f371b982c2bea12ecf6028491
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-05 08:39:45 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_29.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void separator();
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	separator();
+
+	return 0;
+}
+
+void separator()
+{
+	cout << "***********************";
+}
