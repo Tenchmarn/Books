d85dc4329207a2f2e0d020be530428b82b569c0b
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-08 08:30:40 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_031.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int checkArmstrongNumber(int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	cout << "Введите положительное число: ";
+	cin >> n;
+
+	if (checkArmstrongNumber(n))
+	{
+		cout << "Да, является числом Армстронга.\n";
+	}
+	else
+	{
+		cout << "Нет, не является числом Армстронга.\n";
+	}
+
+	return 0;
+}
+
+
+int checkArmstrongNumber(int number)
+{
+	int originalNumber, remainder, result = 0, n(0), flag;
+
+	originalNumber = number;
+
+	// выявление кол-ва цифр
+	while (originalNumber != 0)
+	{
+		originalNumber /= 10;
+		++n;
+	}
+
+	originalNumber = number;
+
+	while (originalNumber != 0)
+	{
+		remainder = originalNumber % 10;
+		result += pow(remainder, n);
+		originalNumber /= 10;
+	}
+
+	if (result == number)
+	{
+		flag = 1;
+	}
+	else
+	{
+		flag = 0;
+	}
+
+	return flag;
+}
