36cb52c1e7eeb506de64d5a9315a607637ac83dc
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-09 08:27:45 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_032.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int checkPrimeNumber(int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n1, n2;
+
+	bool flag;
+
+	cout << "Введите два положительных числа: ";
+	cin >> n1 >> n2;
+
+	cout << "Простые числа между " << n1 << " и " << n2 << ": ";
+	for (int i = n1 + 1; i < n2; ++i)
+	{
+		// Если i - простое число, flag = 1;
+		flag = checkPrimeNumber(i);
+
+		if (flag)
+		{
+			cout << i << " ";
+		}
+	}
+
+	cout << endl;
+
+	return 0;
+}
+
+
+int checkPrimeNumber(int n)
+{
+	bool flag = true;
+
+	for (int j = 2; j <= n / 2; ++j)
+	{
+		if (n % j == 0)
+		{
+			flag = false;
+			break;
+		}
+	}
+
+	return flag;
+}
