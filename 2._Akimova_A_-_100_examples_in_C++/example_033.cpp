4adde7787a372159f190cfbd1a6412a73b729224
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-09 08:28:00 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_033.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool checkPrime(int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, i;
+
+	bool flag = false;
+
+	cout << "Введите положительное число: ";
+	cin >> n;  // 56
+
+	for (i = 2; i <= n / 2; ++i)     // 28
+	{
+		if (checkPrime(i))
+		{
+			if (checkPrime(n - i))
+			{
+				cout << n << " = " << i << " + " << n - i << endl;
+				flag = true;
+			}
+		}
+
+	}
+
+	if (!flag)
+	{
+		cout << n << " не может быть выражено как сумма двух простых чисел\n";
+	}
+
+	return 0;
+}
+
+	// проверка на простое число
+bool checkPrime(int n)
+{
+	int i;
+	bool isPrime = true;
+
+	for (i = 2; i <= n / 2; ++i)      
+	{
+		if (n % 2 == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+
+	return isPrime;
+}
