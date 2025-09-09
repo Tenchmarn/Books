9a3d690e6b2cbcaafa7b64fe78143b70f38de45e
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-09 08:28:28 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_034.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	cout << "Введите положительное число: ";
+	cin >> n;
+
+	cout << "Факториал " << n << " = " << factorial(n) << endl;
+
+	return 0;
+}
+
+int factorial(int n) 
+{
+	if (n > 1) 
+	{
+		return n * factorial(n - 1);
+	}
+	else
+	{
+		return 1;
+	}
+}
