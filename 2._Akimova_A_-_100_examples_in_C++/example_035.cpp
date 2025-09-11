0e5493d95508b673aa6ca57bda2fd062c6a89ba2
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-11 07:49:35 +0300

    Add files via upload

---
+++ 2._Akimova_A_-_100_examples_in_C++/example_035.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int add(int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n; 
+
+	cout << "Введите положительное целове число: ";
+	cin >> n;
+
+	cout << "Сумма = " << add(n) << endl;
+
+	return 0;
+}
+
+int add(int n)
+{
+	if (n != 0)
+	{
+		return n + add(n - 1); // 5 + 4 + 3 + 2 + 1 + 0
+	}
+	return 0;
+}
