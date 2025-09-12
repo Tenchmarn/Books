96206a9e8bef9b38e69776cffbe3083396134ace
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-12 03:20:34 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/partaray.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int SIZE = 4;
+
+struct part
+{
+public:
+	int modelnumber;
+	int partnumber;
+	float cost;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n;
+
+	part apart[SIZE];
+
+	for (n = 0; n < SIZE; n++)
+	{
+		cout << endl;
+
+		cout << "Введите номер модели: ";
+		cin >> apart[n].modelnumber;
+
+		cout << "Введите номер части: ";
+		cin >> apart[n].partnumber;
+
+		cout << "Введите стоимость: ";
+		cin >> apart[n].cost;
+	}
+
+	cout << endl;
+
+	for (n = 0; n < SIZE; n++)
+	{
+		cout << "Модель " << apart[n].modelnumber;
+		cout << " Часть " << apart[n].partnumber;
+		cout << " Стоимость " << apart[n].cost << endl;
+	}
+
+	cout << endl;
+
+
+	return 0;
+}
