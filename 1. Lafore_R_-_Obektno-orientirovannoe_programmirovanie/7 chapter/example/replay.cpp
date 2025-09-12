a9b118d09630ad0a3c5e24e21844e241e4362859
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-12 03:20:49 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/replay.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int age[4];
+
+	for (int j = 0; j < 4; j++)
+	{
+		cout << "Введите возраст: ";
+		cin >> age[j];
+	}
+
+	for (int j = 0; j < 4; j++)
+	{
+		cout << "Вы ввели возраст: " << age[j] << endl;
+	}
+
+	return 0;
+}
