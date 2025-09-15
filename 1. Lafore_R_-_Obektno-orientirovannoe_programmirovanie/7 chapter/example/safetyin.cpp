284ea0a424f03c07fa5771c731c9b59ee6cf03f1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-15 06:32:57 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/safetyin.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	const int MAX = 5;
+	char str[MAX];
+
+	cout << "\nВведите строку: ";
+
+	cin >> setw(MAX) >> str;
+
+	cout << "Вы ввели: " << str << endl;
+
+	return 0;
+}
