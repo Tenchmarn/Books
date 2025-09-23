6dc9af0b9448e644efdc60ff9482d7c75b130f12
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-23 07:46:15 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/sstrfind.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	string s1 = "В лесу родилась елочка, в лесу она росла.";
+	int n;
+
+	n = s1.find("елочка");
+	cout << "Елочка найдена: " << n << endl;
+
+	n = s1.find_first_of("умка");
+	cout << "Первый из умка: " << n << endl;
+
+	n = s1.find_first_not_of("абвгдАБВГД");
+	cout << "Первый не из: " << n << endl;
+
+	return 0;
+}
