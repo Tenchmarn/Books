0224237edf3c4408d6f441c7dd6f7632cb7d5c6c
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-23 07:45:01 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/sstrchng.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	string s1("Все хорошо, прекрасная маркиза.");
+	string s2("принцесса");
+	string s3("Приветствую ");
+
+	s1.erase(0, 12);				// убираем диапазон символов
+	s1.replace(11, 7, s2);			// замена диапазона  
+	s1.insert(0, s3);				// вставление объекта в диапазон
+	s1.erase(s1.size() - 1, 1);		// убираем знак "."
+	s1.append(3, '!');
+
+	cout << "s1: " << s1 << endl;
+
+	int x = s1.find(' ');
+	while (x < s1.size())
+	{
+		s1.replace(x, 1, "/");
+		x = s1.find(' ');
+	}
+
+	cout << "s1: " << s1 << endl;
+
+
+
+	return 0;
+}
