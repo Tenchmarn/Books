463bb758e7764c306326f2ebdabdf76212d9babb
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-25 22:18:36 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/straray.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	const int DAYS = 12;		// кол-во строк в массиве.
+	const int MAX = 30;		// максимальная длина каждой из них.
+
+	char star[DAYS][MAX] =
+	{
+		"Понедельник", "Вторник", "Среда", "Четверг",
+		"Пятница", "Суббота", "Воскресенье"
+	};
+
+	for (int j = 0; j < DAYS; j++)
+	{
+		cout << star[j] << endl;
+	}
+
+
+
+	return 0;
+}
