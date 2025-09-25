e39681f8b6a282d1795019f6fee7105d9a290df5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-25 22:17:46 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/sstrio.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	string full_name, nickname, address;
+	string greeting("Здравствуйте, ");
+
+	cout << "Введите Ваше имя (ФИО): ";
+	getline(cin, full_name);
+	cout << "Ваше имя: " << full_name << endl;
+
+	cout << "Введите Ваш псевдоним: ";
+	cin >> nickname;
+
+	greeting += nickname;
+	cout << greeting << endl;
+
+	cout << "Введите Ваш адрес в несколько строк\n";
+	cout << "Окончание ввода символ '$'\n";
+
+	getline(cin, address, '$');
+	cout << "Ваш адресс: " << address << endl;
+
+	return 0;
+}
