dad6a309cba2d7dc550e3711af8219701877443d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2025-09-25 22:19:05 +0300

    Add files via upload

---
+++ 1. Lafore_R_-_Obektno-orientirovannoe_programmirovanie/7 chapter/example/strcopy1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstring>		// дает метод копирования строки
+
+using namespace std;
+
+int j;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char str1[] = "Маленькой елочке холодно зимой.";
+
+	const int MAX = 80;
+	char str2[MAX];
+
+	for (j = 0; j < strlen(str1); j++)
+	{
+		str2[j] = str1[j];
+	
+	}
+
+	str2[j] = '\0';
+
+
+	cout << str2 << endl;
+
+
+	return 0;
+}
